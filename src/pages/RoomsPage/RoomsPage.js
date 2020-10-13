f279927c2a6c911e16f9c918519b7935950f6345
Niklas Kiuru <niklas.kiuru1@gmail.com>
2020-10-13 14:49:35 +0300

    Add API calls

--- src/pages/RoomsPage/RoomsPage.js
+++ src/pages/RoomsPage/RoomsPage.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getHospitals, getRooms} from "../../utils/api";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const RoomsPage = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    getHospitals('hospital0')
+      .then((result) => {
+        console.log(result);
+        setIsLoaded(true);
+        setRooms(result.rooms);
+      },
+      (err) => {
+        setError(true);
+        setIsLoaded(true);
+        console.log(err)
+      })
+  }, []);
   return (
-    <div>
-      Rooms
+    <div style={{paddingTop: '16px'}}>
+      <p>Rooms</p>
+      {!isLoaded ? <CircularProgress color="secondary"/> :
+        (rooms.map(room => (
+          <li key={room.id}>
+            {room.name}
+          </li>
+        )))
+      }
+      {error && (
+        <p>ERROR</p>
+      )}
     </div>
   )
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,3 +2,65 @@ export const authenticated = () => {
   return true;
   //return localStorage.getItem('token') !== null;
 };
+
+const API_KEY = 'zL43mXgXk5xa7YFRBVZscbLnGFaqVh24q5G6fhGjmAv532FAVBRtnuCJpwXWXnhw';
+const BASE_URL = 'https://cleaner-app-api.azurewebsites.net/api/';
+
+export const getRooms = async (hospital, floor) => {
+  const url = 'rooms';
+  let params = hospital ? `?hospital_id=${hospital}` : '';
+  params += floor ? `?floor_id=${floor}` : '';
+  return doGetRequest(url, params);
+}
+
+export const getHospitals = async () => {
+  return doGetRequest('hospitals');
+}
+
+export const startCleaning = async (roomId) => {
+  return doGetRequest(`room/startcleaning?id=${roomId}`);
+}
+
+export const stopCleaning = async (roomId) => {
+  return doGetRequest(`room/stopcleaning?id=${roomId}`);
+}
+
+export const getAssignedRooms = async (cleaner) => {
+  return doGetRequest(`cleaners/rooms?${cleaner}`);
+}
+
+export const assignRoom = async (room, cleaner) => {
+  return doPostRequest('cleaners/rooms', JSON.stringify({
+    'cleaner_id': cleaner,
+    'room_id': room
+  }));
+}
+
+const doGetRequest = async (url, params) => {
+  const requestURL = BASE_URL + url + (params || '');
+  console.log(requestURL);
+  const response = await fetch(requestURL, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': API_KEY,
+      'charset': 'utf-8'
+    }
+  });
+  return response.json();
+}
+
+const doPostRequest = async (url, params) => {
+  const requestURL = BASE_URL + url;
+  console.log(requestURL);
+  const response = await fetch(requestURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': API_KEY,
+      'charset': 'utf-8'
+    },
+    body: params
+  });
+  return response.json();
+}
