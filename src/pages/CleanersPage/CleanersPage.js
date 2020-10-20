de59a81ae956b2abf0f8b877895dea3e94a2601c
Niklas Kiuru <niklas.kiuru1@gmail.com>
2020-10-20 13:10:36 +0300

    Add form validation for cleaner creation

--- src/pages/CleanersPage/CleanersPage.js
+++ src/pages/CleanersPage/CleanersPage.js
@@ -1,22 +1,31 @@
 import React, {useEffect, useState} from 'react';
 import {useHistory} from "react-router-dom";
-import {Grid, IconButton, Paper, Table, TableContainer, TableHead, TableRow, Typography} from "@material-ui/core";
+import {
+  Grid,
+  IconButton,
+  Paper,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  Tooltip,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Button,
+  DialogActions,
+  InputLabel,
+  MenuItem
+} from "@material-ui/core";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
 import {AddCircleOutline, VisibilityOutlined} from "@material-ui/icons";
 import {createCleaner, getCleaners} from "../../utils/api";
 import styles from './CleanersPage.module.scss';
-import Tooltip from "@material-ui/core/Tooltip";
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import DialogActions from "@material-ui/core/DialogActions";
-import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
+import * as Yup from "yup";
+import {Formik, Form, Field} from "formik";
+import {Select, TextField} from "formik-material-ui";
 
 const CleanersPage = () => {
   const [cleaners, setCleaners] = useState([]);
@@ -49,101 +58,120 @@ const CleanersPage = () => {
   )
 }
 
-const AddCleanerDialog = ({open, setOpen, setIsLoaded}) => {
-  const [first, setFirst] = useState('');
-  const [last, setLast] = useState('');
-  const [startHour, setStartHour] = useState('8');
-  const [startMinutes, setStartMinutes] = useState('00');
-  const [endHour, setEndHour] = useState('16');
-  const [endMinutes, setEndMinutes] = useState('00');
+const validationSchema = Yup.object({
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required()
+});
 
+const AddCleanerDialog = ({open, setOpen, setIsLoaded}) => {
   const handleClose = () => {
     setOpen(false);
   }
 
-  const handleSubmit = async () => {
-    const cleaner = await createCleaner(`${first} ${last}`,
-      `${startHour.padStart(2, '0')}:${startMinutes.padStart(2, '0')}`,
-      `${endHour.padStart(2, '0')}:${endMinutes.padStart(2, '0')}`);
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+    startHour: '8',
+    endHour: '16',
+    startMinutes: '00',
+    endMinutes: '00'
+  }
+
+  const handleSubmit = async (vars, {setSubmitting}) => {
+    const cleaner = await createCleaner(`${vars.firstName} ${vars.lastName}`,
+      `${vars.startHour.padStart(2, '0')}:${vars.startMinutes.padStart(2, '0')}`,
+      `${vars.endHour.padStart(2, '0')}:${vars.endMinutes.padStart(2, '0')}`);
     console.log(cleaner);
     setIsLoaded(false);
+    setSubmitting(false);
     setOpen(false);
   }
 
   return (
     <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
       <DialogTitle id="form-dialog-title">Create a new cleaner</DialogTitle>
-      <DialogContent>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="First name"
-              type="text"
-              value={first}
-              onChange={(event => setFirst(event.target.value))}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              margin="dense"
-              id="name"
-              label="Last name"
-              type="text"
-              value={last}
-              onChange={(event => setLast(event.target.value))}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs>
-            <InputLabel id="startHour">HH</InputLabel>
-            <Select labelId="startHour" value={startHour} onChange={event => setStartHour(event.target.value)}
-                    MenuProps={{classes: {paper: styles.menu}}}>
-              {[...Array(24).keys()].map((number) => {
-                return <MenuItem key={number} value={number.toString()}>{number}</MenuItem>;
-              })}
-            </Select>
-          </Grid>
-          <Grid item xs>
-            <InputLabel id="startMinutes">MM</InputLabel>
-            <Select labelId="startMinutes" value={startMinutes} onChange={event => setStartMinutes(event.target.value)}
-                    MenuProps={{classes: {paper: styles.menu}}}>
-              {['00', 15, 30, 45].map((number) => {
-                return <MenuItem key={number} value={number.toString()}>{number}</MenuItem>;
-              })}
-            </Select>
-          </Grid>
-          <Grid item xs>
-            <InputLabel id="endHour">HH</InputLabel>
-            <Select labelId="endHour" value={endHour} onChange={event => setEndHour(event.target.value)}
-                    MenuProps={{classes: {paper: styles.menu}}}>
-              {[...Array(23).keys()].map((number) => {
-                return <MenuItem key={number} value={number.toString()}>{number}</MenuItem>;
-              })}
-            </Select>
-          </Grid>
-          <Grid item xs>
-            <InputLabel id="endMinutes">MM</InputLabel>
-            <Select labelId="endMinutes" value={endMinutes} onChange={event => setEndMinutes(event.target.value)}
-                    MenuProps={{classes: {paper: styles.menu}}}>
-              {['00', 15, 30, 45].map((number) => {
-                return <MenuItem key={number} value={number.toString()}>{number}</MenuItem>;
-              })}
-            </Select>
-          </Grid>
-        </Grid>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={handleSubmit} color="primary">
-          Create cleaner
-        </Button>
-      </DialogActions>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+        {({isSubmitting, resetForm, values}) => (
+          <Form>
+            <DialogContent>
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <Field
+                    component={TextField}
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="First name"
+                    type="text"
+                    name="firstName"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <Field
+                    component={TextField}
+                    margin="dense"
+                    id="name"
+                    label="Last name"
+                    type="text"
+                    name="lastName"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs>
+                  <InputLabel id="startHour">HH</InputLabel>
+                  <Field component={Select}
+                         inputProps={{labelId: "startHour", menuProps: {classes: {paper: styles.menu}}}}
+                         name="startHour">
+                    {[...Array(24).keys()].map((number) => {
+                      return <MenuItem key={number} value={number.toString()}>{number}</MenuItem>;
+                    })}
+                  </Field>
+                </Grid>
+                <Grid item xs>
+                  <InputLabel id="startMinutes">MM</InputLabel>
+                  <Field component={Select}
+                         inputProps={{labelId: "startMinutes", menuProps: {classes: {paper: styles.menu}}}}
+                         name="startMinutes">
+                    {['00', 15, 30, 45].map((number) => {
+                      return <MenuItem key={number} value={number.toString()}>{number}</MenuItem>;
+                    })}
+                  </Field>
+                </Grid>
+                <Grid item xs>
+                  <InputLabel id="endHour">HH</InputLabel>
+                  <Field component={Select}
+                         inputProps={{labelId: "endHour", menuProps: {classes: {paper: styles.menu}}}}
+
+                         name="endHour">
+                    {[...Array(23).keys()].map((number) => {
+                      return <MenuItem key={number} value={number.toString()}>{number}</MenuItem>;
+                    })}
+                  </Field>
+                </Grid>
+                <Grid item xs>
+                  <InputLabel id="endMinutes">MM</InputLabel>
+                  <Field component={Select}
+                         inputProps={{labelId: "endMinutes", menuProps: {classes: {paper: styles.menu}}}}
+                         name="endMinutes">
+                    {['00', 15, 30, 45].map((number) => {
+                      return <MenuItem key={number} value={number.toString()}>{number}</MenuItem>;
+                    })}
+                  </Field>
+                </Grid>
+              </Grid>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button type="submit" color="primary">
+                Create cleaner
+              </Button>
+            </DialogActions>
+          </Form>
+        )}
+      </Formik>
     </Dialog>
   );
 }
