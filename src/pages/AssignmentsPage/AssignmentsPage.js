9158a52b5c500c9c396b33a5110452be71fc15c8
Niklas Kiuru <niklas.kiuru1@gmail.com>
2020-10-20 12:36:22 +0300

    Reorder imports

--- src/pages/AssignmentsPage/AssignmentsPage.js
+++ src/pages/AssignmentsPage/AssignmentsPage.js
@@ -2,24 +2,25 @@ import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import {lighten, makeStyles} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import {Button} from "@material-ui/core";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
+  Toolbar,
+  Typography,
+  Paper,
+  Checkbox,
+  IconButton,
+  Tooltip,
+  Button
+} from '@material-ui/core';
 import {assignRoomsToCleaner, getRooms} from "../../utils/api";
-import {VisibilityOutlined} from "@material-ui/icons";
+import {VisibilityOutlined, FilterList} from "@material-ui/icons";
 import moment from "moment";
 import {useHistory} from 'react-router-dom';
 import CircularProgress from "@material-ui/core/CircularProgress";
@@ -174,7 +175,7 @@ const EnhancedTableToolbar = (props) => {
           <IconButton aria-label="filter list" onClick={() => {
             // TODO implement filters
           }}>
-            <FilterListIcon/>
+            <FilterList/>
           </IconButton>
         </Tooltip>
       )}
@@ -218,7 +219,6 @@ export default function AssignmentsPage() {
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [roomsPerPage, setRoomsPerPage] = React.useState(5);
-  const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [rooms, setRooms] = useState([]);
   const history = useHistory();
@@ -245,7 +245,6 @@ export default function AssignmentsPage() {
           setIsLoaded(true);
         },
         (error) => {
-          setError(true);
           setIsLoaded(true);
           console.log(error);
         });
