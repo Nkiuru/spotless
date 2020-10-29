efa81b498e99d562cd6126d331a02e22c4242ca8
Niklas Kiuru <niklas.kiuru1@gmail.com>
2020-10-29 13:08:27 +0200

    build

---
+++ build/precache-manifest.9fa19683eda80ddcff7218971aa73876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e687871ba32115236be0b38e72e967fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "512062b5fff3b6b5e310",
+    "url": "/static/css/main.f06a1495.chunk.css"
+  },
+  {
+    "revision": "3a7ce704e8b11f201b2d",
+    "url": "/static/js/2.83a6b463.chunk.js"
+  },
+  {
+    "revision": "095788bd5d236a5e3d23d9ae38287d38",
+    "url": "/static/js/2.83a6b463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512062b5fff3b6b5e310",
+    "url": "/static/js/main.fec75d52.chunk.js"
+  },
+  {
+    "revision": "b222eb2c343c3e4f1edc",
+    "url": "/static/js/runtime-main.ea570ad2.js"
+  }
+]);
