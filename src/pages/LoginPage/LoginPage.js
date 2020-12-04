95c2feeda07c2c55323cc60016b0557ad402e6e0
Niklas Kiuru <niklas.kiuru1@gmail.com>
2020-12-04 13:25:47 +0200

    Add logo to login

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -8,7 +8,7 @@ import {authenticate} from "../../utils/api";
 import {useHistory} from 'react-router-dom';
 import Paper from "@material-ui/core/Paper";
 import styles from './LoginPage.module.scss';
-
+import logo from "../../assets/web_logo.png";
 
 const validationSchema = Yup.object({
   username: Yup.string().required('Username is required'),
@@ -31,45 +31,52 @@ const LoginPage = () => {
 
   return (
     <PageContainer noToolbar>
-      <Paper variant={"elevation"} className={styles.form}>
-        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
-          {({isSubmitting, resetForm, values}) => (
-            <Form>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <Typography variant={"h4"}>Login to spotless</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Field
-                    component={TextField}
-                    autoFocus
-                    variant={"outlined"}
-                    margin="dense"
-                    id="name"
-                    label="Username"
-                    type="text"
-                    name="username"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Field
-                    component={TextField}
-                    variant={"outlined"}
-                    margin="dense"
-                    id="name"
-                    label="Password"
-                    type="password"
-                    name="password"
-                    fullWidth
-                  />
-                </Grid>
-              </Grid>
-              <Button type="submit" color="primary" variant={"contained"} className={styles.btn}>Sign in</Button>
-            </Form>
-          )}
-        </Formik>
-      </Paper>
+      <Grid container direction={"row"} spacing={2} className={styles.content}>
+        <Grid item xs={12}>
+          <img src={logo} alt="LeviTeZer" className={styles.img}/>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper variant={"elevation"} className={styles.form}>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+              {({isSubmitting, resetForm, values}) => (
+                <Form>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <Typography variant={"h4"}>Login to Spotless</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Field
+                        component={TextField}
+                        autoFocus
+                        variant={"outlined"}
+                        margin="dense"
+                        id="name"
+                        label="Username"
+                        type="text"
+                        name="username"
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Field
+                        component={TextField}
+                        variant={"outlined"}
+                        margin="dense"
+                        id="name"
+                        label="Password"
+                        type="password"
+                        name="password"
+                        fullWidth
+                      />
+                    </Grid>
+                  </Grid>
+                  <Button type="submit" color="primary" variant={"contained"} className={styles.btn}>Sign in</Button>
+                </Form>
+              )}
+            </Formik>
+          </Paper>
+        </Grid>
+      </Grid>
     </PageContainer>
   )
 }
